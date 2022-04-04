83036c47e12edded0d72e24c6dd6411b2379174b
Erkan Cicek <erkan.ec.cicek@gmail.com>
2022-04-04 13:17:57 +0200

    erkan

--- CinemaManagerSystem/src/main/java/com/Cinema/CinemaManagerSystem/Controller/SalonController.java
+++ CinemaManagerSystem/src/main/java/com/Cinema/CinemaManagerSystem/Controller/SalonController.java
@@ -31,15 +31,10 @@ public class SalonController { // Toros
     }
 
     @GetMapping("/downloadOneSalon")
-    public String downloadOneSalon(@RequestParam(value = "salonID", defaultValue = "-1") int salonID, @RequestParam(value = "cinemaId") int cinemaId){
+    public String downloadOneSalon(@RequestParam(value = "salonID", defaultValue = "-1") int salonID, @RequestParam(value = "cinemaID") int cinemaId){
         return salonService.downloadOneSalonByID(salonID, cinemaId);
     }
 
-    @GetMapping("/test")
-    public String test(@RequestParam("id")int id){
-        Gson gson = new Gson();
-        return gson.toJson(salonService.test(id));
-    }
 
 
     // necessary ??
