771bbf5b43fba668e0934cfaaf00ee3d4dbbb18d
Lisa Duente <lisa.duente@gmail.com>
2022-04-13 15:05:41 +0200

    Lisa: added RequestParameters in ReservationController

--- CinemaManagerSystem/src/main/java/com/Cinema/CinemaManagerSystem/Controller/ReservationController.java
+++ CinemaManagerSystem/src/main/java/com/Cinema/CinemaManagerSystem/Controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.Cinema.CinemaManagerSystem.Service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,12 +14,17 @@ public class ReservationController {
     ReservationService service;
 
     @PostMapping("/makeReservation")
-    public void makeReservation(String seats, int salonID, String row, int movieID, String time, String date){
+    public void makeReservation(@RequestParam(value = "seats", defaultValue = "0,0")String seats,
+                                @RequestParam(value = "salonID", defaultValue = "1") int salonID,
+                                @RequestParam(value = "row", defaultValue = "1") String row,
+                                @RequestParam(value = "movieID", defaultValue = "1") int movieID,
+                                @RequestParam(value = "time", defaultValue = "00:00")String time,
+                                @RequestParam(value = "date", defaultValue = "2022-01-01") String date){
         service.makeReservation(seats,salonID,row,movieID,time,date);
     }
 
     @GetMapping("/getReservation")
-    public String getReservation(int ID){
+    public String getReservation(@RequestParam(value = "ID", defaultValue = "1") int ID){
         return service.getReservation(ID);
     }
 
